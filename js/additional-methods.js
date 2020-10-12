da1319132d40c61429026d8c1156b726bd238547
Roy <573755374@qq.com>
2020-10-12 15:47:35 +0800

    更新了红包的一些错误提示问题

--- js/additional-methods.js
+++ js/additional-methods.js
@@ -1,36 +1,32 @@
-jQuery.validator.addMethod("isupimg", function(value, element,params) {
-  console.log($('.cover-img').attr('src')); 
-  if($('.cover-img').attr('src')){
+jQuery.validator.addMethod("isupimg", function (value, element, params) {
+  console.log($('.cover-img').attr('src'));
+  if ($('.cover-img').attr('src')) {
     return true
-  }else{
+  } else {
     return false
   }
 }, "请上传封面");
 
-jQuery.validator.addMethod("allmoney", function(value, element,params) {
-  console.log(typeof $('input[name="money"]').val() );
-  if($('input[name="money"]').val()>=0.01){
-    return true
-  }else{
-    return false
-  }
-}, "总金额不可低于0.01");
 
-jQuery.validator.addMethod("singlemoney", function(value, element,params) {
-  var single=parseFloat($('input[name="money"]').val())/parseFloat($('input[name="num"]').val())
-  console.log(single);
-  if(single>=0.01){
-    return true
-  }else{
-    return false
-  }
-}, "单个红包金额不可低于0.01");
 
-jQuery.validator.addMethod("zhumoney", function(value, element,params) {
-  
-  if(parseFloat($('input[name="money"]').val())<=parseFloat($.cookie('zhumoney'))){
+jQuery.validator.addMethod("zhumoney", function (value, element, params) {
+
+  if (parseFloat($('input[name="money"]').val()) <= parseFloat($.cookie('zhumoney'))) {
     return true
-  }else{
+  } else {
     return false
   }
 }, "您的钱包余额不足，请充值后重试");
+
+
+jQuery.validator.addMethod("minNumber", function (value, element) {
+  var returnVal = true;
+  var ArrMen = $('input[name="money"]').val().split(".");    //截取字符串
+  if (ArrMen.length == 2) {
+    if (ArrMen[1].length > 2) {    //判断小数点后面的字符串长度
+      returnVal = false;
+      return false;
+    }
+  }
+  return returnVal;
+}, "小数点后最多为两位");         //验证错误信息
